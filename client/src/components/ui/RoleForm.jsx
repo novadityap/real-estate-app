11aca6c5257c360921ebc43bcb1e5c6dc3cad1e9
novadityap <novaaditya23@gmail.com>
2025-05-17 07:38:19 +0700

    Add RoleForm component and integrate it into Role page for role management

---
+++ client/src/components/ui/RoleForm.jsx
@@ -0,0 +1,69 @@
+import { Button } from '@/components/shadcn/button';
+import { Input } from '@/components/shadcn/input';
+import useFormHandler from '@/hooks/useFormHandler';
+import {
+  Form,
+  FormField,
+  FormLabel,
+  FormMessage,
+  FormItem,
+  FormControl,
+} from '@/components/shadcn/form';
+import { TbLoader } from 'react-icons/tb';
+
+const RoleForm = ({
+  initialValues,
+  mutation,
+  onComplete,
+  onCancel,
+  isCreate,
+}) => {
+  const { form, handleSubmit, isLoading } = useFormHandler({
+    formType: 'datatable',
+    ...(!isCreate && { params: [{ name: 'roleId', value: initialValues.id }] }),
+    mutation,
+    onComplete,
+    defaultValues: {
+      name: initialValues.name ?? '',
+    },
+  });
+
+  return (
+    <Form {...form}>
+      <form className="space-y-4" onSubmit={handleSubmit}>
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <div className="flex justify-end gap-x-2">
+          <Button variant="secondary" type="button" onClick={onCancel}>
+            Cancel
+          </Button>
+          <Button type="submit" disabled={isLoading}>
+            {isLoading ? (
+              <>
+                <TbLoader className="animate-spin" />
+                {isCreate ? 'Creating..' : 'Updating..'}
+              </>
+            ) : isCreate ? (
+              'Create'
+            ) : (
+              'Update'
+            )}
+          </Button>
+        </div>
+      </form>
+    </Form>
+  );
+};
+
+export default RoleForm;
---
+++ client/src/pages/private/Role.jsx
@@ -0,0 +1,53 @@
+import { createColumnHelper } from '@tanstack/react-table';
+import DataTable from '@/components/ui/DataTable';
+import {
+  useSearchRolesQuery,
+  useLazyShowRoleQuery,
+  useCreateRoleMutation,
+  useUpdateRoleMutation,
+  useRemoveRoleMutation,
+} from '@/services/roleApi';
+import RoleForm from '@/components/ui/RoleForm';
+import BreadcrumbNav from '@/components/ui/BreadcrumbNav';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/shadcn/card';
+
+const Role = () => {
+  const columnHelper = createColumnHelper();
+  const columns = [
+    columnHelper.accessor('name', {
+      header: 'Name',
+      size: 100,
+    }),
+  ];
+
+  return (
+    <>
+      <BreadcrumbNav />
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-gray-600">Roles</CardTitle>
+          <CardDescription>Manage roles</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <DataTable
+            columns={columns}
+            searchQuery={useSearchRolesQuery}
+            lazyShowQuery={useLazyShowRoleQuery}
+            createMutation={useCreateRoleMutation}
+            updateMutation={useUpdateRoleMutation}
+            removeMutation={useRemoveRoleMutation}
+            FormComponent={RoleForm}
+            entityName="role"
+          />
+        </CardContent>
+      </Card>
+    </>
+  );
+};
+export default Role;
